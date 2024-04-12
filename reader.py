813310e5ed42f297cce9f63907d634ef7d9fa812
SITS-Web <web@sitservices.org>
2024-04-12 09:48:54 -0400

    edit readme.md

--- reader.py
+++ reader.py
@@ -1,6 +1,6 @@
 """
 plan for implementation of reader.py below:
-1) implement screen reader agent
+1) implement computer vision agent
 2) create functions for agent to read snake movements
 3) turn snake movements into data
 4) connect with neural network
